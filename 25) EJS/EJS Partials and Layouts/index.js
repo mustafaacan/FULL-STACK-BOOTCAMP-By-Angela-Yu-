4652e43960db9925c2707f37a0081f435dd682ba
Mustafa Can <mustafaa.can.3535@gmail.com>
2026-07-16 23:46:39 +0300

    EJS

---
+++ 25) EJS/EJS Partials and Layouts/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("./public"));
+
+// To read HTML form data
+app.use(express.urlencoded({ extended: true }));
+
+// To read JSON data
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  const data = req.body;
+  console.log("Entered Name: ", data["name"]);
+  console.log("Entered Email: ", data["email"]);
+  console.log("Entered Text: ", data["text"]);
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
