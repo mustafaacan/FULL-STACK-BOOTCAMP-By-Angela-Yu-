9eba3a6e659d6ab174d3cccbc0296dcf8fb5b55b
Mustafa Can <mustafaa.can.3535@gmail.com>
2026-07-28 00:52:12 +0300

    API

--- 28)API/4_REST_APIs/index.js
+++ 28)API/4_REST_APIs/index.js
@@ -6,13 +6,25 @@ const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
+// CAUTION: DEFAULT VALUES CANNOT BE CHANGED SO WORK ON THE VALUE THAT YOU NEWLY CREATED ON THE SYSTEM.
+// IN OTHER WORDS, BEFORE USING PUT, PATCH AND DELETE, CREATE A VALUE WITH USING POST
+
 // HINTs: Use the axios documentation as well as the video lesson to help you.
 // https://axios-http.com/docs/post_example
 // Use the Secrets API documentation to figure out what each route expects and how to work with it.
 // https://secrets-api.appbrewery.com/
 
-//TODO 1: Add your own bearer token from the previous lesson.
-const yourBearerToken = "";
+axios.interceptors.request.use((request) => {
+  console.log("Headers:", request.headers);
+  console.log("Auth config:", request.auth);
+  console.log("Query params:", request.params);
+  console.log("Request URL:", request.url);
+  console.log("Request Method:", request.method);
+  console.log("Request Body:", request.data);
+  return request;
+});
+
+const yourBearerToken = "38764063-c377-490b-9cc1-523c1692e350";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
@@ -24,32 +36,103 @@ app.get("/", (req, res) => {
 });
 
 app.post("/get-secret", async (req, res) => {
-  const searchId = req.body.id;
-  try {
-    const result = await axios.get(API_URL + "/secrets/" + searchId, config);
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
-  } catch (error) {
-    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  const searchId = req.body.id.trim();
+  if (searchId) {
+    try {
+      const result = await axios.get(API_URL + "/secrets/" + searchId, config);
+      res.render("index.ejs", { content: JSON.stringify(result.data) });
+    } catch (error) {
+      res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
+  } else {
+    res.render("index.ejs", {
+      content: "Search ID should not be empty and should be a number.",
+    });
   }
 });
 
 app.post("/post-secret", async (req, res) => {
-  // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+  const secret = req.body.secret.trim();
+  const score = req.body.score.trim();
+  if (secret && score) {
+    try {
+      const result = await axios.post(
+        API_URL + "/secrets",
+        { secret: secret, score: score },
+        config,
+      );
+      res.render("index.ejs", { content: JSON.stringify(result.data) });
+    } catch (error) {
+      res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
+  } else {
+    res.render("index.ejs", {
+      content: "Both secret and score should be provided",
+    });
+  }
 });
 
 app.post("/put-secret", async (req, res) => {
   const searchId = req.body.id;
-  // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+  const secret = req.body.secret.trim();
+  const score = req.body.score.trim();
+  if (secret && score && searchId) {
+    try {
+      const result = await axios.put(
+        `${API_URL}/secrets/${searchId}`,
+        { secret: secret, score: score },
+        config,
+      );
+      res.render("index.ejs", { content: JSON.stringify(result.data) });
+    } catch (error) {
+      res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
+  } else {
+    res.render("index.ejs", {
+      content: "SearchID, secret and score should be provided",
+    });
+  }
 });
 
 app.post("/patch-secret", async (req, res) => {
   const searchId = req.body.id;
-  // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
+  const secret = req.body.secret.trim();
+  const score = req.body.score.trim();
+  if ((secret || score) && searchId) {
+    try {
+      const result = await axios.patch(
+        `${API_URL}/secrets/${searchId}`,
+        { secret: secret, score: score },
+        config,
+      );
+      res.render("index.ejs", { content: JSON.stringify(result.data) });
+    } catch (error) {
+      res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
+  } else {
+    res.render("index.ejs", {
+      content: "secret or score (or both) should be provided with searchID",
+    });
+  }
 });
 
 app.post("/delete-secret", async (req, res) => {
   const searchId = req.body.id;
-  // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
+  if (searchId) {
+    try {
+      const result = await axios.delete(
+        `${API_URL}/secrets/${searchId}`,
+        config,
+      );
+      res.render("index.ejs", { content: JSON.stringify(result.data) });
+    } catch (error) {
+      res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
+  } else {
+    res.render("index.ejs", {
+      content: "searchID should be provided with searchID",
+    });
+  }
 });
 
 app.listen(port, () => {
