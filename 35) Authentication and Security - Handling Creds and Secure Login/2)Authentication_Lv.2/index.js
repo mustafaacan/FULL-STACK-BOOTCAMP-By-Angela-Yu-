aabbcecfc9d333bacf3e59b5b9d9876a84f2ac49
Mustafa Can <mustafaa.can.3535@gmail.com>
2026-09-16 04:13:14 +0300

    Authentication & Security

--- 35) Authentication and Security - Handling Creds and Secure Login/2)Authentication_Lv.2/index.js
+++ 35) Authentication and Security - Handling Creds and Secure Login/2)Authentication_Lv.2/index.js
@@ -1,16 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
 
 // YAPILMADI. INCELE VE YAPMAYA ÇALIŞ.
+// KULLANICIYA ŞİFRE ZAYIF MI GÜÇLÜ MÜ UYARISI VEREN BİR MEKANİZMA KURABİLİR MİYİZ
+//
+// 2. MIDDLEWARE OLARAK KULLAN, EĞER KULLANICI ADI VE ŞİFRE KRİTERLERE UYUYORSA
+// 2. MIDDLEWARE ŞİFRE İÇERİĞİNİ İNCELESİN
 
 const app = express();
 const port = 3000;
+// How many salting steps will be applied
+const saltRounds = 10;
 
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "secrets",
+  database: "Secrets",
   password: "123456",
   port: 5432,
 });
@@ -43,12 +50,20 @@ app.post("/register", async (req, res) => {
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in.");
     } else {
-      const result = await db.query(
-        "INSERT INTO users (email, password) VALUES ($1, $2)",
-        [email, password],
-      );
-      console.log(result);
-      res.render("secrets.ejs");
+      // PASSWORD HASHING
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.log("Error while password hashing process", err);
+        } else {
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2)",
+            // ENTER HASH INSTEAD OF PASSWORD
+            [email, hash],
+          );
+          console.log(result);
+          res.render("secrets.ejs");
+        }
+      });
     }
   } catch (err) {
     console.log(err);
@@ -66,12 +81,22 @@ app.post("/login", async (req, res) => {
     if (result.rows.length > 0) {
       const user = result.rows[0];
       const storedPassword = user.password;
-
-      if (password === storedPassword) {
-        res.render("secrets.ejs");
-      } else {
-        res.send("Incorrect Password");
-      }
+      /*
+      CAUTION : Since hashed password will be change everytime, we cannot
+      compare the entered original password with the recorded one so functions
+      include their own compare methods for it
+       */
+      bcrypt.compare(password, storedPassword, async (err, result) => {
+        if (err) {
+          console.log("Error while comparing passwords: ", err);
+        }
+        // if passowrd is correct, result will be true
+        if (result) {
+          return res.render("secrets.ejs");
+        } else {
+          return res.send("Incorrect Password or Email");
+        }
+      });
     } else {
       res.send("User not found");
     }
